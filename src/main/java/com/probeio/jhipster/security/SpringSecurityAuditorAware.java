cd5ac428ff85d687100a43a999c63cebb7d34d82
probe <re2rar@gmail.com>
2020-05-29 12:36:07 +0900

    Initial version of jhipster generated by JHipster-6.9.0

---
+++ src/main/java/com/probeio/jhipster/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package com.probeio.jhipster.security;
+
+import com.probeio.jhipster.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
